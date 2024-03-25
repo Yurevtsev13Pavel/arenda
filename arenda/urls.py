f0710ce741a427e71a7ceab31cbb31d54984b235
“Yurevtsev13Pavel” <paulprozxc@mail.ru>
2024-03-25 22:06:47 +0500

    report +/-

--- arenda/urls.py
+++ arenda/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from crm import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('auth.urls', namespace='auth'))
+    path('auth/', include('auth.urls', namespace='auth')),
+    path('', views.first_page, name='main_page'),
+    path('thanks/', views.thanks_page, name='thanks_page'),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOt)
